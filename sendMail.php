8606af52366ea257018338688d5542fa87602f53
Juan Escobar <juanescobar.90@gmail.com>
2017-03-08 23:30:00 -0500

    probando correo

--- sendMail.php
+++ sendMail.php
@@ -1,31 +1,28 @@
 <?php
 require_once('PHPMailer/class.phpmailer.php');
+
 $correo = new PHPMailer(); //Creamos una instancia en lugar usar mail()
-$postdata = file_get_contents("php://input");
-$request = json_decode($postdata);
-@$name = $request->name;
-@$email = $request->email;
-@$company = $request->company;
-@$message = $request->message;
-
-$correo->SetFrom($email, "");
-
+//Usamos el SetFrom para decirle al script quien envia el correo
+$correo->SetFrom("juanescobar.90@gmail.com", "");
+ 
 //Usamos el AddReplyTo para decirle al script a quien tiene que responder el correo
-$correo->AddReplyTo($_POST["correo"],"");
+$correo->AddReplyTo("juanescobar.90@gmail.com","");
  
 //Usamos el AddAddress para agregar un destinatario
-$correo->AddAddress("juanescobar.90@gmail.com", "Robot");
+$correo->AddAddress("desarrollo@studentshouse.com.co", "Robot");
  
 //Ponemos el asunto del mensaje
 $correo->Subject = "Contacto";
-$messages = '
-	<h2>Just sent an email with the form Contact Our Company</h2> <br>
-	Name: '.$name.'<br>
-	Email: '.$email.'<br>
-	Company: '.$company.'<br>
-	Message: '.$message.'<br>';
 
-$correo->MsgHTML($messages);
+/*
+ * Si deseamos enviar un correo con formato HTML utilizaremos MsgHTML:
+ * $correo->MsgHTML("<strong>Mi Mensaje en HTML</strong>");
+ * Si deseamos enviarlo en texto plano, haremos lo siguiente:
+ * $correo->IsHTML(false);
+ * $correo->Body = "Mi mensaje en Texto Plano";
+ */
+
+$correo->MsgHTML("asdasdasd");
  
 //Si deseamos agregar un archivo adjunto utilizamos AddAttachment
 //$correo->AddAttachment("images/phpmailer.gif");
@@ -36,5 +33,4 @@ if(!$correo->Send()) {
 } else {
   echo "<b>Mensaje enviado con exito. <br>Espera nuestra respuesta.</b>";
 }
-
 ?>
